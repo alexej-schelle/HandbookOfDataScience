81a47a53d95bf7fa9ffeb3f678c800424056eff7
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2024-12-17 13:46:13 +0100

    Add files via upload

---
+++ GraphQL/src/error/index.ts
@@ -0,0 +1,11 @@
+export { GraphQLError, printError, formatError } from './GraphQLError';
+export type {
+  GraphQLErrorOptions,
+  GraphQLFormattedError,
+  GraphQLErrorExtensions,
+  GraphQLFormattedErrorExtensions,
+} from './GraphQLError';
+
+export { syntaxError } from './syntaxError';
+
+export { locatedError } from './locatedError';
