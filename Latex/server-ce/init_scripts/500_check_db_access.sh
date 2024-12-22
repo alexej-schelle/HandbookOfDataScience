85f19de7048ea1595d82cbab71980583a4142378
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2024-12-22 17:19:13 +0100

    Delete Latex directory

--- Latex/server-ce/init_scripts/500_check_db_access.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Checking can connect to mongo and redis"
-cd /overleaf/services/web
-node modules/server-ce-scripts/scripts/check-mongodb
-node modules/server-ce-scripts/scripts/check-redis
-echo "All checks passed"
